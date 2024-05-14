555202fb21ce00f43af24878ea72b0f8d2ce7eef
Negrie17 <220757534@ump.ac.za>
2024-05-14 15:33:10 +0200

    My first commit

---
+++ src/main/java/com/mycompany/devops_gitbash/Motsetsen220757534.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.devops_gitbash;
+
+/**
+ *
+ * @author Student
+ */
+public class Motsetsen220757534 {
+
+    public static void main(String[] args) {
+        System.out.println("I Love Java");
+    }
+}
